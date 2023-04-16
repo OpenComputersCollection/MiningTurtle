38712df92b61f2ea719a13f4d3ddf8d8072fac76
Teddy55Codes <teddy@teddy.email>
2023-04-16 02:21:33 +0200

    input mining dimensions

---
+++ MiningTurtle.lua
@@ -0,0 +1,8 @@
+local robot = require("robot")
+local term = require("term")
+term.write("x: ")
+xDimension = term.read()
+term.write("y: ")
+yDimension = term.read()
+term.write("z: ")
+zDimension = term.read()
