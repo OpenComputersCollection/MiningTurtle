fad7dab7d93b4aa2f8985d7a697fa24c31d7ca1e
Teddy55Codes <teddy@teddy.email>
2023-04-23 20:08:59 +0200

    robot now returns and deposits if inventory is full or battery is low

--- MiningTurtle.lua
+++ MiningTurtle.lua
@@ -1,5 +1,6 @@
 local robot = require("robot")
 local term = require("term")
+local computer = require("computer")
 term.write("x: ")
 xDimension = term.read()
 term.write("y: ")
@@ -10,9 +11,14 @@ zDimension = term.read()
 currentX = 0
 currentY = 0
 currentZ = 0
+currentlyReturning = false
 
 directions = { "N", "E", "S", "W" }
 currentDirectionIndex = 0
+
+function deepCopy(x)
+    return x
+end
 
 function turn(isLeftTurn)
     if isLeftTurn then
@@ -24,28 +30,130 @@ function turn(isLeftTurn)
     end
 end
 
+function shouldReturn()
+    local totalEnergy = computer.maxEnergy()
+    local currentEnergy = computer.energy()
+    local energyPercentage = (currentEnergy / totalEnergy) * 100
+
+    if energyPercentage < 10 then
+        return true
+    end
+
+    local isInventoryFull = true
+    for i = 1, robot.inventorySize() do
+        if robot.count(i) == 0 then
+            isInventoryFull = false
+            break
+        end
+    end
+
+    return isInventoryFull
+end
+
 function move()
-    if (directions[currentDirectionIndex % 4] == "N") then
+    robot.forward()
+    if (directions[(currentDirectionIndex % 4)+1] == "N") then
         currentX = currentX + 1
-    elseif (directions[currentDirectionIndex % 4] == "E") then
-        currentY = currentY - 1
-    elseif (directions[currentDirectionIndex % 4] == "S") then
-        currentX = currentX - 1
-    elseif (directions[currentDirectionIndex % 4] == "W") then
+    elseif (directions[(currentDirectionIndex % 4)+1] == "E") then
         currentY = currentY + 1
+    elseif (directions[(currentDirectionIndex % 4)+1] == "S") then
+        currentX = currentX - 1
+    elseif (directions[(currentDirectionIndex % 4)+1] == "W") then
+        currentY = currentY - 1
+    end
+
+    if (not currentlyReturning) and shouldReturn() then
+        currentlyReturning = true
+        local lastMiningX = deepCopy(currentX)
+        local lastMiningY = deepCopy(currentY)
+        local lastMiningZ = deepCopy(currentZ)
+        local lastDirection = deepCopy(currentDirectionIndex)
+        returnToOrigin()
+        returnToLastMiningPosition(lastMiningX, lastMiningY, lastMiningZ, lastDirection)
+        currentlyReturning = false
     end
 end
 
-turnIterator = 0
+function dropItems()
+    for i = 1, robot.inventorySize() do
+        robot.select(i)
+        robot.drop()
+    end
+end
+
+function returnToOrigin()
+
+    while currentZ ~= 0 do
+        while robot.detectUp() do
+            robot.swingUp()
+        end
+        robot.up()
+        currentZ = currentZ - 1
+    end
+
+    while directions[(currentDirectionIndex % 4)+1] ~= "W" do
+        turn(false)
+    end
+
+    while currentY ~= 0 do
+        while robot.detect() do
+            robot.swing()
+        end
+        move()
+    end
+
+    turn(true)
+
+    while currentX ~= 0 do
+        while robot.detect() do
+            robot.swing()
+        end
+        move()
+    end
+
+    dropItems()
+
+    turn(false)
+    turn(false)
+end
+
+function returnToLastMiningPosition(lastMiningX, lastMiningY, lastMiningZ, lastDirection)
+    while currentX ~= lastMiningX do
+        while robot.detect() do
+            robot.swing()
+        end
+        move()
+    end
+
+    turn(false)
+
+    while currentY ~= lastMiningY do
+        while robot.detect() do
+            robot.swing()
+        end
+        move()
+    end
+
+    while currentZ ~= lastMiningZ do
+        while robot.detectDown() do
+            robot.swingDown()
+        end
+        robot.down()
+        currentZ = currentZ + 1
+    end
+
+    while currentDirectionIndex % 4 ~= lastDirection % 4 do
+        turn(false)
+    end
+end
 
 for iz = zDimension, 1, -1 do
-    xTurnIterator = 0
+    turnIterator = 0
     for iy = yDimension, 1, -1 do
         for ix = xDimension - 1, 1, -1 do
             while robot.detect() do
                 robot.swing()
             end
-            robot.forward()
             move()
         end
 
@@ -55,7 +163,6 @@ for iz = zDimension, 1, -1 do
                 while robot.detect() do
                     robot.swing()
                 end
-                robot.forward()
                 move()
                 turn(false)
             else
@@ -63,7 +170,6 @@ for iz = zDimension, 1, -1 do
                 while robot.detect() do
                     robot.swing()
                 end
-                robot.forward()
                 move()
                 turn(true)
             end
@@ -81,3 +187,5 @@ for iz = zDimension, 1, -1 do
         turn(true)
     end
 end
+
+returnToOrigin()
